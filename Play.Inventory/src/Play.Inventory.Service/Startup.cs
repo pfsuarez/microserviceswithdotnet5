0337d7b2dc7af8871820864c2c9892fc0dcf7b26
Pedro Suarez <pfsuarez@gmail.com>
2021-07-29 18:55:53 +0200

    Implementing retries with exponential backoff

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -5,11 +5,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 namespace Play.Inventory.Service
 {
@@ -25,6 +27,7 @@ namespace Play.Inventory.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Random jitterer = new();
 
             services.AddMongo()
                     .AddMongoRepository<InventoryItem>("inventory-items");
@@ -34,6 +37,15 @@ namespace Play.Inventory.Service
                 {
                     client.BaseAddress = new Uri("https://localhost:5001");
                 })
+                .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+                    5,
+                    retryAttemp => TimeSpan.FromSeconds(Math.Pow(2, retryAttemp)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+                    onRetry: (outcome, timespan, retryAttemp) =>
+                    {
+                        var serviceProvider = services.BuildServiceProvider();
+                        serviceProvider.GetService<ILogger<CatalogClient>>()?.LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttemp}");
+                    }
+                ))
                 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1))
                 .ConfigurePrimaryHttpMessageHandler(() =>
                 {
