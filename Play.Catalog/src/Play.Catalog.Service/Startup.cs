a059b2e6b08ee681e9b7e84e8a90d4d4a8033abf
Pedro Suarez <pfsuarez@gmail.com>
2021-08-03 10:01:51 +0200

    Refactoring MassTransit configuration into the reusable NuGet package

--- Play.Catalog/src/Play.Catalog.Service/Startup.cs
+++ Play.Catalog/src/Play.Catalog.Service/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Settings;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
 using Play.Common.Settings;
 
@@ -30,20 +30,8 @@ namespace Play.Catalog.Service
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
             services.AddMongo()
-                    .AddMongoRepository<Item>("items");
-
-            services.AddMassTransit(x =>
-            {
-                x.UsingRabbitMq((context, configurator) =>
-                {
-                    var rabbitMQSettings = Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                    configurator.Host(rabbitMQSettings.Host);
-                    configurator.ConfigureEndpoints(context, 
-                        new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-                });
-            });
-            
-            services.AddMassTransitHostedService();
+                    .AddMongoRepository<Item>("items")
+                    .AddMassTransitWithRabbitMq();
 
             services.AddControllers(options =>
             {
