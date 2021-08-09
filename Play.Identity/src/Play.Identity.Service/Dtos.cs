96c219c040e7eccfe7fe464cbe2d33bebc7ffaff
Pedro Suarez <pfsuarez@gmail.com>
2021-08-09 17:02:46 +0200

    Adding the Users REST API

---
+++ Play.Identity/src/Play.Identity.Service/Dtos.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Play.Identity.Service.Dtos
+{
+    public record UserDto(Guid Id,
+                          string UserName,
+                          string Email,
+                          decimal Gil,
+                          DateTimeOffset CreatedDate);
+
+    public record UpdateUserDto([Required][EmailAddress]string Email,
+                                [Range(0, 1000000)]decimal Gil);
+}
